b8d57e1ec0cfcfff6f093d311548ca041cb899c5
jkobell <k4kobell@gmail.com>
2021-12-04 11:18:00 -0600

    commit_added_track_changes_to_firebase_db

---
+++ js/ui.js
@@ -0,0 +1,19 @@
+const commentsList = document.querySelector('#comments_list');
+
+const renderComment = (data, id) => {
+    const html = `
+    <li class="commentslist" data-id ="${id}">
+        <span>Name: ${data.friendlyname}
+        </span>
+        <span>Email:  ${data.email}
+        </span>
+        <span>Region: ${data.region}
+        </span>
+        <span>Comment: ${data.comment}
+        </span>
+    </li>
+    `;
+      
+    commentsList.innerHTML += html;
+  };
+  
